cd95378dfc04ddd11f74f9c407813c019c9316e0
Amelia Brown <amelia.brown389@gmail.com>
2016-03-08 01:47:04 -0500

    (feature): add click on and off event to view modal

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -5,7 +5,11 @@ import styles from './gallery-styles';
 @CSSModules(styles)
 
 export default class Gallery extends Component {
-  state = {data: []};
+  state = {
+    data: [],
+    modal: "",
+  };
+
   componentWillMount() {
     const xhttp = new XMLHttpRequest();
     xhttp.open('GET', 'https://api.imgur.com/3/gallery/hot/viral/0.json');
@@ -18,25 +22,37 @@ export default class Gallery extends Component {
     }.bind(this);
     xhttp.send();
   }
+
   render () {
-    var elements = this.state.data.map(function(item, index) {
-      return <div styleName={item.is_album === true ? "blank" : "imgrContainer"} key={item.id}><img src={item.link} styleName="imgrImg" /></div>;
+    var elements = this.state.data.map((item, index) => {
+      return (
+        <div
+          onClick={::this.handleClick}
+          data-link={item.link}
+          styleName={item.is_album === true ? "blank" : "imgrContainer"}
+          key={item.id}>
+         <img
+          src={item.link}
+          styleName="imgrImg" />
+       </div>
+      );
     })
     return (
       <div className={styles.classbase}
        styleName="galleryContainer">
         {elements}
+        <div
+          styleName={this.state.modal === "" ? "modalHidden" : "modalShowing"}
+          onClick={::this.handleClickOff}>
+          <img src={this.state.modal} styleName="modal" />
+        </div>
       </div>
-    )
+    );
+  }
+  handleClick(e) {
+    return this.setState({modal: e.target.dataset.link});
+  }
+  handleClickOff(e) {
+    return this.setState({modal: ""});
   }
 }
-
-/*
-xhttp.onreadystatechange = function() {
-  if (xhttp.readyState == 4 && xhttp.status == 200) {
-    document.getElementById("demo").innerHTML = xhttp.responseText;
-  }
-};
-xhttp.open("GET", "ajax_info.txt", true);
-xhttp.send();
- * */
